cdd838e54e192a3b0231fb1a7a833b9f2ceccd6b
parmar10preeti-ctrl <parmar10preeti@gmail.com>
2025-11-13 13:05:00 +0000

    class added

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+data class Destination(
+    val name: String,
+    val singlePrice: Double,
+    val returnPrice: Double,
+    var takings: Double = 0.0
+) {
+    fun addTakings(amount: Double) {
+        takings += amount
     }
 }
+
+data class Ticket(
+    val origin: String,
+    val destination: Destination,
+    val type: String,  // "Single" or "Return"
+    val price: Double
+) {
+    fun printTicket() {
+        println("\n***")
+        println(origin)
+        println("to")
+        println(destination.name)
+        println("Price: £${"%.2f".format(price)} [$type]")
+        println("***\n")
+    }
+}
+
